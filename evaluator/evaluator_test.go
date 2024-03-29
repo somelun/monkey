7ae891cbc73d3fee4cc5d2ad846b93d5b9207162
Sasha <somelun@gmail.com>
2024-03-29 23:28:29 +0000

    Evaluate booleans

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -9,17 +9,17 @@ import (
 
 func TestEvalIntegerExpression(t *testing.T) {
 	tests := []struct {
-		input string
-    	expected int64
-    }{
-    	{"5", 5},
-     	{"10", 10},
-    }
+		input    string
+		expected int64
+	}{
+		{"5", 5},
+		{"10", 10},
+	}
 
-    for _, tt := range tests {
-    	evaluated := testEval(tt.input)
-     	testIntegerObject(t, evaluated, tt.expected)
-    }
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testIntegerObject(t, evaluated, tt.expected)
+	}
 }
 
 func testEval(input string) object.Object {
@@ -42,5 +42,33 @@ func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
 		return false
 	}
 
+	return true
+}
+
+func TestEvalBooleanExpression(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected bool
+	}{
+		{"true", true},
+		{"false", false},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testBooleanObject(t, evaluated, tt.expected)
+	}
+}
+
+func testBooleanObject(t *testing.T, obj object.Object, expected bool) bool {
+	result, ok := obj.(*object.Boolean)
+	if !ok {
+		t.Errorf("object is not Boolean. got=%T (%+v)", obj, obj)
+		return false
+	}
+	if result.Value != expected {
+		t.Errorf("object has wrong value. got=%t, want=%t", result.Value, expected)
+		return false
+	}
 	return true
 }

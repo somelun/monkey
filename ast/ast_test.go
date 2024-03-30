7c22e0915cd02b4bc3ce555c4a2ffe7aebd4a32d
Sasha <somelun@gmail.com>
2024-03-30 23:51:33 +0000

    Bindind and enviroment

--- ast/ast_test.go
+++ ast/ast_test.go
@@ -7,19 +7,19 @@ import (
 
 func TestString(t *testing.T) {
 	program := &Program{
-        Statements: []Statement{
-            &LetStatement{
-                Token: token.Token{Type: token.LET, Literal: "let"},
-                Name: &Identifier{
-                    Token: token.Token{Type: token.IDENT, Literal: "myVar"},
-                    Value: "myVar",
-                },
-            },
-        },
-	},
-	Value: &Identifier{
-    	Token: token.Token{Type: token.IDENT, Literal: "anotherVar"},
-     	Value: "anotherVar",
+		Statements: []Statement{
+			&LetStatement{
+				Token: token.Token{Type: token.LET, Literal: "let"},
+				Name: &Identifier{
+					Token: token.Token{Type: token.IDENT, Literal: "myVar"},
+					Value: "myVar",
+				},
+				Value: &Identifier{
+					Token: token.Token{Type: token.IDENT, Literal: "anotherVar"},
+					Value: "anotherVar",
+				},
+			},
+		},
 	}
 
 	if program.String() != "let myVar = anotherVar;" {
